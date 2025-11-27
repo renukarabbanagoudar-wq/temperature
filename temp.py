b55705a05e903dbd9d1b225efcf62e3cb020e08c
Renuka <renuka.rabbanagoudar@gmail.com>
2025-11-27 15:44:32 +0530

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,16 @@
+import sys
+if len(sys.argv)==2:
+  script_name(sys.argv)
+  print("Usage:python temperature_alert.py <temperature-in_C>")
+    sys.exist(1)
+else:
+    temp=float(sys.argv[1])
+expect ValueError:
+    print("Please enter a valid temperature!")
+    sys.exist(1)
+if temp <15:
+  print("Clod")
+elif 15<=temp<=30:
+   print("Normal")
+else:
+  print("Hot")
